e732ae7ecc84c02eb48dbe8ee71dc848362d3f3a
ramzanbhutto <kali.arch.78@gmail.com>
2025-04-04 12:37:18 +0500

    leetcode

---
+++ Decode.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int numDecodings(string );
+int main()
+{
+  string str;
+  cout<<"Enter the string to decode: ";
+  cin>>str;
+  cout<<"The number of ways to decode the string is: "<<numDecodings(str)<<endl;
+
+  return 0;
+}
+int numDecodings(string s) {
+    int n = s.size();
+    if (n == 0 || s[0] == '0') return 0;
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        if (s[i - 1] != '0') {
+            dp[i] += dp[i - 1];
+        }
+        if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')) {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[n];
+}
