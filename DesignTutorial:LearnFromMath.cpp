0630b179debbae2ab408d8da3348dfaab0e7edc2
ramzanbhutto <kali.arch.78@gmail.com>
2025-05-06 22:16:30 +0500

    codeforces

---
+++ DesignTutorial:LearnFromMath.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cin>>num;
+  if(num%2 == 0)
+    cout<<num-8<<" "<<8<<endl;
+  else
+   cout<<num-9<<" "<<9<<endl;
+
+  return 0;
+}
